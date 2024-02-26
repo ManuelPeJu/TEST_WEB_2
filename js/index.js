7a6b9b19413c405c40992d8c23bc267ea03ffd2c
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-02-26 20:57:27 +0100

    navbar work in progress

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// Añade esto al final de tu archivo script.js
+
+document.querySelector('.hamburger-btn').addEventListener('click', function() {
+    document.querySelector('.nav_sidebar').classList.toggle('show-sidebar');
+  });
+  
